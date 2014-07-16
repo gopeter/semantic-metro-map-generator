a29e50c01dc53b24bcd0f1c830ce97d4faea0431
Peter Göbel <peter.goebel@debd.de>
2014-07-16 17:00:22 +0200

    added API calls

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,12 @@
 var SMMG = function() {
+  
+  // API settings
+  this.API = {
+    "base": "http://semantic-metro-map-gtfs-api.herokuapp.com/api/",
+    "agency": "caltrain"
+  };
+  
+  // Init!
   this.init();
 };
 
@@ -6,12 +14,11 @@ SMMG.prototype = {
 
   init: function() {
   
-    this.loadMap();
-  
     // event handler
-    // ...
+    $(document).hammer();
+    $(document).on('tap', 'button', $.proxy(this.generateMap, this));
       
-  }
+  },
   
   /***************************************
   * Helpers 
@@ -21,6 +28,41 @@ SMMG.prototype = {
   * Events
   ***************************************/
   
+  generateMap: function() {
+
+    var self = this;
+
+    // fetch routes
+    $.when(
+      $.ajax({
+        url: self.API.base + 'routes/' + self.API.agency,
+        dataType: 'json'
+      })
+    ).then(function(routes) {
+
+      // for each route ...
+      $.each(routes, function(i, route) {
+      
+        // ... fetch all stops twice for each direction (0/1 for inbound/outbound)
+        for (var direction = 0; direction <= 1; direction++) {
+          $.when(
+            $.ajax({
+              url: self.API.base + 'stops/' + self.API.agency + '/' + route.route_id + '/' + direction,
+              dataType: 'json'
+            })
+          ).then(function(stops) {      
+          
+            // now lets build our mega object!
+            console.log(stops);
+            
+          });        
+        }
+        
+      });
+
+    });
+    
+  }
   
 };
 
